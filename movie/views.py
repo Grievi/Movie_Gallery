9c2a406ccb80367a773da92e7889262ab1009de8
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-14 11:14:01 +0300

    made first request

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    message='Hello world'
+    return render(request, 'movies/index.html',{"message": message})
