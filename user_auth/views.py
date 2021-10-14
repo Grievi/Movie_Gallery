72c7950016f9e37950adf4a1e9cabd8ac806d93f
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-14 12:16:03 +0300

    add login form

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
 
 def user_login(request):
     message = 'Login to Proceed'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
 
-    return render(request, 'authentication/login.html', {"message": message})
+        if user is not None:
+            login(request, user)
+            messages.success(request,f"Welcome {username} to Movie Gallore!")
+            return redirect('home')
+
+        else:
+            messages.success(request,"Oops Somethinge went wrong, please Login!")
+
+            return render(request, 'authentication/login.html')
+    else:
+        return render(request, 'authentication/login.html', {"message": message})
+
+    
